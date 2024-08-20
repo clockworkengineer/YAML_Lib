6ad0386ddee99dc6b19cafbd0e3e9f2ff0e0dece
clockworkengineer <robert_tizzard@hotmail.com>
2024-08-20 12:50:09 +0100

    SNapshot.

--- classes/include/implementation/ynode/YAML_YNode_Reference.hpp
+++ classes/include/implementation/ynode/YAML_YNode_Reference.hpp
@@ -5,8 +5,7 @@ namespace YAML_Lib {
 // =======================
 // What is YNode variant ?
 // =======================
-template<typename T> bool isA(const YNode &yNode)
-{
+template <typename T> bool isA(const YNode &yNode) {
   if constexpr (std::is_same_v<T, String>) {
     return yNode.getVariant().getNodeType() == Variant::Type::string;
   } else if constexpr (std::is_same_v<T, Number>) {
@@ -21,6 +20,8 @@ template<typename T> bool isA(const YNode &yNode)
     return yNode.getVariant().getNodeType() == Variant::Type::null;
   } else if constexpr (std::is_same_v<T, Hole>) {
     return yNode.getVariant().getNodeType() == Variant::Type::hole;
+  } else if constexpr (std::is_same_v<T, Comment>) {
+    return yNode.getVariant().getNodeType() == Variant::Type::comment;
   } else {
     return false;
   }
@@ -29,42 +30,55 @@ template<typename T> bool isA(const YNode &yNode)
 // =========================
 // YNode reference converter
 // =========================
-template<typename T> void checkYNode(const YNode &yNode)
-{
+template <typename T> void checkYNode(const YNode &yNode) {
   if constexpr (std::is_same_v<T, String>) {
-    if (!isA<T>(yNode)) { throw YNode::Error("YNode not a string."); }
+    if (!isA<T>(yNode)) {
+      throw YNode::Error("YNode not a string.");
+    }
   } else if constexpr (std::is_same_v<T, Number>) {
-    if (!isA<T>(yNode)) { throw YNode::Error("YNode not a number."); }
+    if (!isA<T>(yNode)) {
+      throw YNode::Error("YNode not a number.");
+    }
   } else if constexpr (std::is_same_v<T, Array>) {
-    if (!isA<T>(yNode)) { throw YNode::Error("YNode not an array."); }
+    if (!isA<T>(yNode)) {
+      throw YNode::Error("YNode not an array.");
+    }
   } else if constexpr (std::is_same_v<T, Object>) {
-    if (!isA<T>(yNode)) { throw YNode::Error("YNode not an object."); }
+    if (!isA<T>(yNode)) {
+      throw YNode::Error("YNode not an object.");
+    }
   } else if constexpr (std::is_same_v<T, Boolean>) {
-    if (!isA<T>(yNode)) { throw YNode::Error("YNode not a boolean."); }
+    if (!isA<T>(yNode)) {
+      throw YNode::Error("YNode not a boolean.");
+    }
   } else if constexpr (std::is_same_v<T, Null>) {
-    if (!isA<T>(yNode)) { throw YNode::Error("YNode not a null."); }
-  }else if constexpr (std::is_same_v<T, Hole>) {
-    if (!isA<T>(yNode)) { throw YNode::Error("YNode not a hole."); }
+    if (!isA<T>(yNode)) {
+      throw YNode::Error("YNode not a null.");
+    }
+  } else if constexpr (std::is_same_v<T, Hole>) {
+    if (!isA<T>(yNode)) {
+      throw YNode::Error("YNode not a hole.");
+    }
+  } else if constexpr (std::is_same_v<T, Comment>) {
+    if (!isA<T>(yNode)) {
+      throw YNode::Error("YNode not a comment.");
+    }
   }
 }
-template<typename T> T &YRef(YNode &yNode)
-{
+template <typename T> T &YRef(YNode &yNode) {
   checkYNode<T>(yNode);
   return static_cast<T &>(yNode.getVariant());
 }
-template<typename T> const T &YRef(const YNode &yNode)
-{
+template <typename T> const T &YRef(const YNode &yNode) {
   checkYNode<T>(yNode);
   return static_cast<const T &>(yNode.getVariant());
 }
-template<typename T> T &YRef(Object::Entry &yNodeEntry)
-{
+template <typename T> T &YRef(Object::Entry &yNodeEntry) {
   checkYNode<T>(yNodeEntry.getYNode());
   return static_cast<T &>(yNodeEntry.getYNode().getVariant());
 }
-template<typename T> const T &YRef(const Object::Entry &yNodeEntry)
-{
+template <typename T> const T &YRef(const Object::Entry &yNodeEntry) {
   checkYNode<T>(yNodeEntry.getYNode());
   return static_cast<const T &>(yNodeEntry.getYNode().getVariant());
 }
-}// namespace YAML_Lib
+} // namespace YAML_Lib
