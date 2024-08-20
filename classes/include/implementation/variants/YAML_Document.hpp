479902cde3e868866ee6905132821c1f36e5126f
clockworkengineer <robert_tizzard@hotmail.com>
2024-08-20 16:30:38 +0100

    Snapshot.

--- classes/include/implementation/variants/YAML_Document.hpp
+++ classes/include/implementation/variants/YAML_Document.hpp
@@ -8,38 +8,38 @@ struct Document : Variant
   using Entry = YNode;
   using Entries = std::vector<Entry>;
   // Constructors/Destructors
-  Document() : Variant(Type::array) {}
+  Document() : Variant(Type::array) {} // document treated as array so passes indexing checks
   Document(const Document &other) = default;
   Document &operator=(const Document &other) = default;
   Document(Document &&other) = default;
   Document &operator=(Document &&other) = default;
   ~Document() = default;
-  // Add array element
+  // Add document element
   void add(Entry yNode) { yNodeDocument.emplace_back(std::move(yNode)); }
-  // Return the size of array
+  // Return the size of document
   [[nodiscard]] std::size_t size() const { return yNodeDocument.size(); }
-  // Return reference to array base
+  // Return reference to document base
   Entries &value() { return yNodeDocument; }
   [[nodiscard]] const Entries &value() const { return yNodeDocument; }
-  // Document indexing operators
-  YNode &operator[](const std::size_t index)
-  {
-    if (index < yNodeDocument.size()) { return yNodeDocument[index]; }
-    throw YNode::Error("Invalid index used to access array.");
-  }
-  const YNode &operator[](const std::size_t index) const
-  {
-    if (index < yNodeDocument.size()) { return yNodeDocument[index]; }
-    throw YNode::Error("Invalid index used to access array.");
-  }
-  // Resize Document
-  void resize(const std::size_t index)
-  {
-    yNodeDocument.resize(index + 1);
-    for (auto &entry : yNodeDocument) {
-      if (entry.isEmpty()) { entry = YNode::make<Hole>(); }
-    }
-  }
+  // // Document indexing operators
+  // YNode &operator[](const std::size_t index)
+  // {
+  //   if (index < yNodeDocument.size()) { return yNodeDocument[index]; }
+  //   throw YNode::Error("Invalid index used to access document.");
+  // }
+  // const YNode &operator[](const std::size_t index) const
+  // {
+  //   if (index < yNodeDocument.size()) { return yNodeDocument[index]; }
+  //   throw YNode::Error("Invalid index used to access document.");
+  // }
+  // // Resize Document
+  // void resize(const std::size_t index)
+  // {
+  //   yNodeDocument.resize(index + 1);
+  //   for (auto &entry : yNodeDocument) {
+  //     if (entry.isEmpty()) { entry = YNode::make<Hole>(); }
+  //   }
+  // }
 
 private:
   // Document entries list
