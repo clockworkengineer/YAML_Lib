954b18d2e5c8ccef3cb79817bf4970f3594fcc89
clockworkengineer <robert_tizzard@hotmail.com>
2024-08-20 16:56:28 +0100

    SNapshot.

--- tests/source/parse/YAML_Lib_Tests_YAML_Parse_Comment.cpp
+++ tests/source/parse/YAML_Lib_Tests_YAML_Parse_Comment.cpp
@@ -40,6 +40,7 @@ TEST_CASE("Check YAML Parsing of comments.", "[YAML][Parse][Comment]") {
     BufferSource source{"---# Comment 1\n   - 'One'\n# Comment 2\n"};
     REQUIRE_NOTHROW(yaml.parse(source));
     REQUIRE(YRef<Comment>(yaml.root()[0][0]).value() == " Comment 1");
+    REQUIRE_FALSE(!isA<Array>(yaml.root()[0][1]));
     REQUIRE(YRef<Comment>(yaml.root()[0][2]).value() == " Comment 2");
   }
 }
