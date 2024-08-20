f1c09379b0f789e7845f4d779f69e0b9eead23e9
clockworkengineer <robert_tizzard@hotmail.com>
2024-08-20 13:17:11 +0100

    Snapshot.

--- tests/source/parse/YAML_Lib_Tests_YAML_Parse_Comment.cpp
+++ tests/source/parse/YAML_Lib_Tests_YAML_Parse_Comment.cpp
@@ -18,9 +18,22 @@ TEST_CASE("Check YAML Parsing of comments.", "[YAML][Parse][Comment]") {
     BufferSource source{
         "# Comment 1\n---\n# Comment 2\n   - 'One'\n# Comment 3\n"};
     REQUIRE_NOTHROW(yaml.parse(source));
-    // REQUIRE(YRef<Document>(yaml.root()[0]).size() == 0);
     REQUIRE(YRef<Comment>(yaml.root()[0][0]).value() == " Comment 1");
-    // REQUIRE(YRef<Comment>(yaml.root()[0][1]).value() == " Comment 2");
+    REQUIRE(YRef<Comment>(yaml.root()[1][0]).value() == " Comment 2");
+    REQUIRE_FALSE(!isA<Array>(yaml.root()[1][1]));
+    REQUIRE(YRef<Comment>(yaml.root()[1][2]).value() == " Comment 3");
+  }
+  SECTION("YAML Parse of comments and three before start.",
+          "[YAML][Parse][Comment]") {
+    BufferSource source{"# Comment 1\n# Comment 2\n# Comment 3\n---\n# Comment "
+                        "4\n   - 'One'\n# Comment 5\n"};
+    REQUIRE_NOTHROW(yaml.parse(source));
+    REQUIRE(YRef<Comment>(yaml.root()[0][0]).value() == " Comment 1");
+    REQUIRE(YRef<Comment>(yaml.root()[0][1]).value() == " Comment 2");
+    REQUIRE(YRef<Comment>(yaml.root()[0][2]).value() == " Comment 3");
+    REQUIRE(YRef<Comment>(yaml.root()[1][0]).value() == " Comment 4");
+    REQUIRE_FALSE(!isA<Array>(yaml.root()[1][1]));
+    REQUIRE(YRef<Comment>(yaml.root()[1][2]).value() == " Comment 5");
   }
   SECTION("YAML Parse of comments one on the same line as start.",
           "[YAML][Parse][Comment]") {
