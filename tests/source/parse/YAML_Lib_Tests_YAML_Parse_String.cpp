19ca70b41abb04adcc9c38e9065d288ddf1ea866
clockworkengineer <robert_tizzard@hotmail.com>
2024-09-09 10:46:29 +0100

    Snapshot.

--- tests/source/parse/YAML_Lib_Tests_YAML_Parse_String.cpp
+++ tests/source/parse/YAML_Lib_Tests_YAML_Parse_String.cpp
@@ -1,6 +1,6 @@
 #include "YAML_Lib_Tests.hpp"
 
-TEST_CASE("Check YAML Parsing of simple types.", "[YAML][Parse][Simple]") {
+TEST_CASE("Check YAML Parsing of simple types.", "[YAML][Parse][String]") {
   const YAML yaml;
   SECTION("YAML Parse a single quoted string.", "[YAML][Parse][String]") {
     BufferSource source{"---\n'test string.'\n"};
@@ -18,12 +18,12 @@ TEST_CASE("Check YAML Parsing of simple types.", "[YAML][Parse][Simple]") {
     REQUIRE(YAML_Lib::YRef<String>(yaml.root()[0][0]).value() ==
             "test string.");
   }
-  SECTION("YAML Parse a qouted string with some escape sequence in.", "[YAML][Parse][String]") {
-    BufferSource source{"---\n\"test string.\\t.\\n.\\b.\\r.\\f\"\n"};
+  SECTION("YAML Parse a qouted string with some escape sequences in.", "[YAML][Parse][String]") {
+    BufferSource source{"---\n\"test string.\\t.\\n.\\b.\\r.\\f.\\\\.\"\n"};
     REQUIRE_NOTHROW(yaml.parse(source));
     REQUIRE(yaml.getNumberOfDocuments() == 1);
     REQUIRE_FALSE(!isA<String>(yaml.root()[0][0]));
     REQUIRE(YAML_Lib::YRef<String>(yaml.root()[0][0]).value() ==
-            "test string.\t.\n.\b.\r.\f");
+            "test string.\t.\n.\b.\r.\f.\\.");
   }
 }
