f6eef34fa1308aa85a6ec0a865ad601bccb28c59
clockworkengineer <robert_tizzard@hotmail.com>
2024-10-21 17:04:09 +0100

    SNapshot.

---
+++ classes/source/implementation/converter/linux/YAML_Converter.cpp
@@ -0,0 +1,33 @@
+//
+// Class: Converter
+//
+// Description: Convert characters to/from UTF8 and UTF16.
+//
+// Dependencies: C++20 - Language standard features used.
+//
+
+#include <codecvt>
+#include <locale>
+
+#include "YAML_Converter.hpp"
+#include "YAML_Error.hpp"
+
+namespace YAML_Lib {
+
+static std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t> utf16Converter;
+
+/// <summary>
+/// Convert utf8 <-> utf16 strings.
+/// </summary>
+std::u16string toUtf16(const std::string &utf8) 
+{
+  if (utf8.find('\0') != std::string::npos) { throw Error("Tried to convert a null character."); }
+  return utf16Converter.from_bytes(utf8);
+}
+std::string toUtf8(const std::u16string &utf16) 
+{
+  if (utf16.find(u'\0') != std::string::npos) { throw Error("Tried to convert a null character."); }
+  return utf16Converter.to_bytes(utf16);
+}
+
+}// namespace YAML_Lib
---
+++ classes/source/implementation/converter/windows/YAML_Converter.cpp
@@ -0,0 +1,47 @@
+//
+// Class: Converter
+//
+// Description: Convert characters to/from UTF8 and UTF16.
+//
+// Dependencies:  Windows - character conversion API.
+//
+
+#include "Windows.h"
+
+#include "YAML_Converter.hpp"
+#include "YAML_Error.hpp"
+
+namespace YAML_Lib {
+
+// ============================================================
+// Windows API for converting between byte and wide characters.
+// ============================================================
+int WideCharToBytes(wchar_t *wideString, int wideStringLength, char *bytes = nullptr, int length = 0)
+{
+  return (WideCharToMultiByte(CP_UTF8, 0, wideString, wideStringLength, bytes, length, nullptr, nullptr));
+}
+int BytesToWideChar(const char *bytes, int length, wchar_t *sideString = nullptr, int wideStringLength = 0)
+{
+  return (MultiByteToWideChar(CP_UTF8, 0, bytes, length, sideString, wideStringLength));
+}
+
+/// <summary>
+/// Convert utf8 <-> utf16 strings.
+/// </summary>
+std::u16string toUtf16(const std::string &utf8)
+{
+  if (utf8.find('\0') != std::string::npos) { throw Error("Tried to convert a null character."); }
+  std::wstring wideString(BytesToWideChar(utf8.c_str(), static_cast<int>(utf8.length())), 0);
+  BytesToWideChar(utf8.c_str(), static_cast<int>(utf8.length()), &wideString[0], static_cast<int>(wideString.length()));
+  return (std::u16string{ wideString.begin(), wideString.end() });
+}
+
+std::string toUtf8(const std::u16string &utf16)
+{
+  if (utf16.find('\0') != std::string::npos) { throw Error("Tried to convert a null character."); }
+  std::wstring wideString{ utf16.begin(), utf16.end() };
+  std::string bytes(WideCharToBytes(&wideString[0], static_cast<int>(wideString.length())), 0);
+  WideCharToBytes(&wideString[0], -1, &bytes[0], static_cast<int>(bytes.length()));
+  return bytes;
+}
+}// namespace YAML_Lib
