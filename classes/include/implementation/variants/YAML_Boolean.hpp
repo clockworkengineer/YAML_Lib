ffc99facecb70dd16b0f6bfc05decf3a88505643
clockworkengineer <robert_tizzard@hotmail.com>
2024-11-06 09:26:07 +0000

    SNapshot.

--- classes/include/implementation/variants/YAML_Boolean.hpp
+++ classes/include/implementation/variants/YAML_Boolean.hpp
@@ -8,7 +8,7 @@ struct Boolean : Variant
 {
   // Constructors/Destructors
   Boolean() : Variant(Type::boolean) {}
-  Boolean(const bool boolean, const std::string &value) : Variant(Type::boolean), yNodeBoolean(boolean), booleanValue(value) {}
+  Boolean(const bool boolean, const std::string &value) : Variant(Type::boolean), yNodeBoolean(boolean), booleanString(value) {}
   Boolean(const Boolean &other) = default;
   Boolean &operator=(const Boolean &other) = default;
   Boolean(Boolean &&other) = default;
@@ -18,13 +18,13 @@ struct Boolean : Variant
   [[nodiscard]] bool &value() { return yNodeBoolean; }
   [[nodiscard]] const bool &value() const { return yNodeBoolean; }
   // Return string representation of value
-  [[nodiscard]] const std::string &toString() const { return booleanValue; }
+  [[nodiscard]] const std::string &toString() const { return booleanString; }
   // Valid values for true/false
   inline static const std::set<std::string> isTrue{"True", "On", "Yes"};
   inline static const std::set<std::string> isFalse{"False", "Off", "No"};
 
 private:
   bool yNodeBoolean{};
-  std::string booleanValue;
+  std::string booleanString;
 };
 }// namespace YAML_Lib
