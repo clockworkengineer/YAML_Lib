d80371519281d44b3112aa06729b5103b4877b91
clockworkengineer <robert_tizzard@hotmail.com>
2024-12-29 16:21:50 +0000

    Snapshot.

--- tests/source/io/YAML_Lib_Tests_ISource_File.cpp
+++ tests/source/io/YAML_Lib_Tests_ISource_File.cpp
@@ -3,15 +3,15 @@
 TEST_CASE("Check ISource (File) interface.", "[YAML][ISource][File]") {
   const YAML yaml;
   SECTION("Create FileSource.", "[YAML][ISource][File][Construct]") {
-    REQUIRE_NOTHROW(FileSource(prefixPath(kSingleSmallYAMLFile1)));
+    REQUIRE_NOTHROW(FileSource(prefixPath(kSingleSmallYAMLFile)));
   }
   SECTION("Create FileSource from testfile000.yaml and parse",
           "[YAML][ISource][File][Parse]") {
-    REQUIRE_NOTHROW(yaml.parse(FileSource(prefixPath(kSingleSmallYAMLFile1))));
+    REQUIRE_NOTHROW(yaml.parse(FileSource(prefixPath(kSingleSmallYAMLFile))));
   }
   // SECTION("Create FileSource from testfile000.yaml, parse and stringify.",
   //         "[YAML][ISource][File][Parse]") {
-  //   REQUIRE_NOTHROW(yaml.parse(FileSource(prefixPath(kSingleSmallYAMLFile1))));
+  //   REQUIRE_NOTHROW(yaml.parse(FileSource(prefixPath(kSingleSmallYAMLFile))));
   //   compareYAML(yaml, "---\n- 1\n- 1\n- 2\n...\n");
   // }
   SECTION("Check that FileSource position() works correctly.",
@@ -29,14 +29,14 @@ TEST_CASE("Check ISource (File) interface.", "[YAML][ISource][File]") {
   SECTION("Create FileSource and that it is positioned on the correct first "
           "character.",
           "[YAML][ISource][File][Position]") {
-    FileSource source{prefixPath(kSingleSmallYAMLFile1)};
+    FileSource source{prefixPath(kSingleSmallYAMLFile)};
     REQUIRE_FALSE(!source.more());
     REQUIRE(static_cast<char>(source.current()) == '-');
   }
   SECTION("Create FileSource and then check next positions to correct next "
           "character",
           "[YAML][ISource][File][Next]") {
-    FileSource source{prefixPath(kSingleSmallYAMLFile1)};
+    FileSource source{prefixPath(kSingleSmallYAMLFile)};
     source.next();
     source.next();
     source.next();
