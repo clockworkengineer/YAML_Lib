90b804a24e84d4ca5c00de06340c709656b58038
clockworkengineer <robert_tizzard@hotmail.com>
2025-02-12 13:37:06 +0000

    Snapshot.

--- tests/source/number/YAML_Lib_Tests_Number_Precision.cpp
+++ tests/source/number/YAML_Lib_Tests_Number_Precision.cpp
@@ -44,23 +44,23 @@ TEST_CASE("Check JNode Number floating point precision.", "[YAML][JNode][Number]
         Number::setPrecision(6);
         REQUIRE(yamlDestination.toString() == "---\n\"latitude\": 39.06834\n\"longitude\": -70.741615\n...\n");
     }
-    //   SECTION("Floating point precision to maximum.", "[YAML][JNode][Number][Float][Precision]")
-    //   {
-    //     std::string expected{ R"({"latitude":39.068341,"longitude":-70.741615})" };
-    //     BufferSource yamlSource{ expected };
-    //     yaml.parse(yamlSource);
-    //     REQUIRE_FALSE(!YRef<Number>(yaml["latitude"]).is<float>());
-    //     REQUIRE_FALSE(!YRef<Number>(yaml["longitude"]).is<float>());
-    //     BufferDestination yamlDestination;
-    //     Number::setPrecision(std::numeric_limits<long double>::digits10 + 1);
-    //     yaml.stringify(yamlDestination);
-    //     Number::setPrecision(6);
-    //     if constexpr (std::numeric_limits<long double>::digits10 + 1 == 16) {
-    //       REQUIRE(yamlDestination.toString() == R"({"latitude":39.06834030151367,"longitude":-70.74161529541016})");
-    //     } else if constexpr (std::numeric_limits<long double>::digits10 + 1 == 19) {
-    //       REQUIRE(yamlDestination.toString() == R"({"latitude":39.06834030151367188,"longitude":-70.74161529541015625})");
-    //     } else {
-    //       REQUIRE_FALSE(true);
-    //     }
-    //   }
+      SECTION("Floating point precision to maximum.", "[YAML][JNode][Number][Float][Precision]")
+      {
+        std::string expected{ R"({"latitude":39.068341,"longitude":-70.741615})" };
+        BufferSource yamlSource{ expected };
+        yaml.parse(yamlSource);
+        REQUIRE_FALSE(!YRef<Number>(yaml["latitude"]).is<float>());
+        REQUIRE_FALSE(!YRef<Number>(yaml["longitude"]).is<float>());
+        BufferDestination yamlDestination;
+        Number::setPrecision(std::numeric_limits<long double>::digits10 + 1);
+        yaml.stringify(yamlDestination);
+        Number::setPrecision(6);
+        if constexpr (std::numeric_limits<long double>::digits10 + 1 == 16) {
+          REQUIRE(yamlDestination.toString() == "---\n\"latitude\": 39.06834030151367188\n\"longitude\": -70.74161529541015625\n...\n");
+        } else if constexpr (std::numeric_limits<long double>::digits10 + 1 == 19) {
+          REQUIRE(yamlDestination.toString() == "---\n\"latitude\": 39.06834030151367188\n\"longitude\": -70.74161529541015625\n...\n");
+        } else {
+          REQUIRE_FALSE(true);
+        }
+      }
 }
