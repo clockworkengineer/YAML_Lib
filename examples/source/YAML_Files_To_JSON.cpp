54781767921e19e78ba136f7db282334b1d2e1a2
clockworkengineer <robert_tizzard@hotmail.com>
2025-02-21 16:47:40 +0000

    Fix issue with double yaml init.

--- examples/source/YAML_Files_To_JSON.cpp
+++ examples/source/YAML_Files_To_JSON.cpp
@@ -17,20 +17,21 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char **argv) {
     try {
         const yl::YAML yaml(yl::makeStringify<yl::JSON_Stringify>());
         // Initialise logging.
-        init(plog::debug, "YAML_Files_To_YAML.log");
+        init(plog::debug, "YAML_Files_To_JSON.log");
         PLOG_INFO << "YAML_Files_To_YAML started ...";
-        PLOG_INFO << yl::YAML().version();
-        for (const auto &torrentFileName : Utility::createYAMLFileList()) {
-            yaml.parse(yl::FileSource(torrentFileName));
+        PLOG_INFO << yaml.version();
+        for (const auto &yamlFileName : Utility::createYAMLFileList()) {
+            yaml.parse(yl::FileSource(yamlFileName));
             yaml.stringify(yl::FileDestination(
-                Utility::createFileName(torrentFileName, ".json")));
+                Utility::createFileName(yamlFileName, ".json")));
             PLOG_INFO << "Created file "
-                      << Utility::createFileName(torrentFileName, ".json") << " from "
-                      << torrentFileName;
+                      << Utility::createFileName(yamlFileName, ".json") << " from "
+                      << yamlFileName;
+            yaml.parse(yl::FileSource(Utility::createFileName(yamlFileName, ".json")));
         }
     } catch (const std::exception &ex) {
-        PLOG_ERROR << "Error Processing Torrent File: [" << ex.what() << "]\n";
+        PLOG_ERROR << "Error Processing File: [" << ex.what() << "]\n";
     }
-    PLOG_INFO << "YAMLTorrent_Files_To_YAML exited.";
+    PLOG_INFO << "YAML_Files_To_YAML exited.";
     exit(EXIT_SUCCESS);
 }
