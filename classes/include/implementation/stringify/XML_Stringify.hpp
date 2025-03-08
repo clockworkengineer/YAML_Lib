b611117f71d60fc84d660626c42b6488768d0b68
clockworkengineer <robert_tizzard@hotmail.com>
2025-03-08 16:04:27 +0000

    Snapshot.

--- classes/include/implementation/stringify/XML_Stringify.hpp
+++ classes/include/implementation/stringify/XML_Stringify.hpp
@@ -6,8 +6,7 @@
 
 namespace YAML_Lib {
 
-class XML_Stringify : public IStringify
-{
+class XML_Stringify : public IStringify {
 public:
   XML_Stringify() = default;
   XML_Stringify(const XML_Stringify &other) = delete;
@@ -42,41 +41,96 @@ private:
   void stringifyXML(const YNode &yNode, IDestination &destination, const long indent) const
   {
     if (isA<Document>(yNode)) {
-      stringifyXML(YRef<Document>(yNode)[0], destination, indent);
+      stringifyDocument(yNode, destination, indent);
     } else if (isA<Number>(yNode)) {
-      destination.add(std::to_string(YRef<Number>(yNode).value<long long>()));
+      stringifyNumber(yNode, destination);
     } else if (isA<String>(yNode)) {
-      destination.add(xmlTranslator.to(YRef<String>(yNode).value()));
+      stringifyString(yNode, destination);
     } else if (isA<Boolean>(yNode)) {
-      if (YRef<Boolean>(yNode).value()) {
-        destination.add("True");
-      } else {
-        destination.add("False");
-      }
+      stringifyBoolean(yNode, destination);
     } else if (isA<Null>(yNode)||isA<Hole>(yNode)) {
-      ;
     } else if (isA<Dictionary>(yNode)) {
-      for (const auto &yNodeNext : YRef<Dictionary>(yNode).value()) {
-        auto elementName = yNodeNext.getKey();
-        std::ranges::replace(elementName, ' ', '-');
-        destination.add("<" + elementName + ">");
-        stringifyXML(yNodeNext.getYNode(), destination, 0);
-        destination.add("</" + elementName + ">");
-      }
+      stringifyDictionary(yNode, destination);
     } else if (isA<Array>(yNode)) {
-      if (YRef<Array>(yNode).value().size() > 1) {
-        for (const auto &bNodeNext : YRef<Array>(yNode).value()) {
-          destination.add("<Row>");
-          stringifyXML(bNodeNext, destination, 0);
-          destination.add("</Row>");
-        }
-      }
+      stringifyArray(yNode, destination);
     } else {
       throw Error("Unknown YNode type encountered during stringification.");
     }
   }
 
+  void stringifyDocument(const YNode &yNode, IDestination &destination, const long indent) const {
+    stringifyXML(YRef<Document>(yNode)[0], destination, indent);
+  }
+
+  void stringifyNumber(const YNode &yNode, IDestination &destination) const {
+    destination.add(std::to_string(YRef<Number>(yNode).value<long long>()));
+  }
+
+  void stringifyString(const YNode &yNode, IDestination &destination) const {
+    destination.add(xmlTranslator.to(YRef<String>(yNode).value()));
+  }
+
+  void stringifyBoolean(const YNode &yNode, IDestination &destination) const {
+    if (YRef<Boolean>(yNode).value()) {
+      destination.add("True");
+    } else {
+      destination.add("False");
+    }
+  }
+
+  void stringifyNull(const YNode &yNode, IDestination &destination) const {
+
+  }
+
+  void stringifyDictionary(const YNode &yNode, IDestination &destination) const {
+    for (const auto &yNodeNext : YRef<Dictionary>(yNode).value()) {
+      auto elementName = yNodeNext.getKey();
+      std::ranges::replace(elementName, ' ', '-');
+      destination.add("<" + elementName + ">");
+      stringifyXML(yNodeNext.getYNode(), destination, 0);
+      destination.add("</" + elementName + ">");
+    }
+
+  }
+  void stringifyArray(const YNode &yNode, IDestination &destination) const {
+    if (YRef<Array>(yNode).value().size() > 1) {
+      for (const auto &bNodeNext : YRef<Array>(yNode).value()) {
+        destination.add("<Row>");
+        stringifyXML(bNodeNext, destination, 0);
+        destination.add("</Row>");
+      }
+    }
+  }
+
+  // void stringifyDocument(const YNode &yNode, IDestination &destination, const long indent) const {
+  //
+  // }
+  //
+  // void stringifyNumber(const YNode &yNode, IDestination &destination) const {
+  //
+  // }
+  //
+  // void stringifyString(const YNode &yNode, IDestination &destination) const {
+  //
+  // }
+  //
+  // void stringifyBoolean(const YNode &yNode, IDestination &destination) const {
+  //
+  // }
+  //
+  // void stringifyNull(const YNode &yNode, IDestination &destination) const {
+  //
+  // }
+  //
+  // void stringifyDictionary(const YNode &yNode, IDestination &destination) const {
+  //
+  // }
+  // void stringifyAray(const YNode &yNode, IDestination &destination) const {
+  //
+  // }
+
   XML_Translator xmlTranslator;
+
 };
 
 }// namespace YAML_Lib
