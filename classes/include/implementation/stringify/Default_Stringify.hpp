5e3498ca7245bc485d524ae0e270d3d51b1ddd1b
clockworkengineer <robert_tizzard@hotmail.com>
2025-04-15 16:05:23 +0100

    Stringify fixes.

--- classes/include/implementation/stringify/Default_Stringify.hpp
+++ classes/include/implementation/stringify/Default_Stringify.hpp
@@ -15,7 +15,13 @@ public:
   Default_Stringify(Default_Stringify &&other) = delete;
   Default_Stringify &operator=(Default_Stringify &&other) = delete;
   ~Default_Stringify() override = default;
-  // Stringify YNode tree
+  /// <summary>
+  /// Recursively traverse YNode structure encoding it into YAML string on
+  /// the destination stream passed in.
+  /// </summary>
+  /// <param name="yNode">YNode structure to be traversed.</param>
+  /// <param name="destination">Destination stream for stringified YAML.</param>
+  // <param name="indent">Current print indentation.</param>
   void stringify(const YNode &yNode, IDestination &destination,
                  unsigned long indent) const override {
     stringifyYAML(yNode, destination, indent);
@@ -26,124 +32,124 @@ public:
   }
 
 private:
-std::vector<std::string> splitString(const std::string &target,
-                                     const char delimiter) const {
+  std::vector<std::string> splitString(const std::string &target,
+                                       const char delimiter) const {
 
-  std::vector<std::string> splitStrings;
-  if (!target.empty()) {
-    std::stringstream sourceStream(target);
+    std::vector<std::string> splitStrings;
+    if (!target.empty()) {
+      std::stringstream sourceStream(target);
 
-    for (std::string splitOffItem{};
-         std::getline(sourceStream, splitOffItem, delimiter);) {
-      splitStrings.push_back(splitOffItem);
-      splitStrings.back().push_back(delimiter);
-    }
-    splitStrings.back().pop_back();
-  }
-  return splitStrings;
-}
-
-auto calculateIndent(IDestination &destination, const unsigned long indent) const
-  {
-  if (destination.last() == kLineFeed) {
-    return std::string(indent, kSpace);
-  }
-  return std::string("");
-}
-
-void stringifyAnyBlockStyle(IDestination &destination, const YNode &yNode) const{
-  if (isA<String>(yNode)) {
-    if (const auto quote = YRef<String>(yNode).getQuote();
-        quote == '>' || quote == '|') {
-      destination.add("|");
-      destination.add(kLineFeed);
-    }
-  }
-}
-
-void stringifyYAML( const YNode &yNode, IDestination &destination, const unsigned long indent) const {
-  if (isA<Number>(yNode)) {
-    destination.add(YRef<Number>(yNode).toString());
-  } else if (isA<String>(yNode)) {
-    if (const char quote = YRef<String>(yNode).getQuote();
-        quote == kApostrophe || quote == kDoubleQuote) {
-      std::string yamlString{YRef<String>(yNode).toString()};
-      if (quote == kDoubleQuote) {
-        yamlString = yamlTranslator->to(yamlString);
+      for (std::string splitOffItem{};
+           std::getline(sourceStream, splitOffItem, delimiter);) {
+        splitStrings.push_back(splitOffItem);
+        splitStrings.back().push_back(delimiter);
       }
-      destination.add(quote + yamlString + quote);
-    } else {
-      for (const auto &line :
-           splitString(YRef<String>(yNode).toString(), kLineFeed)) {
-        destination.add(calculateIndent(destination, indent));
-        destination.add(line);
+      splitStrings.back().pop_back();
+    }
+    return splitStrings;
+  }
+  auto calculateIndent(IDestination &destination,
+                       const unsigned long indent) const {
+    if (destination.last() == kLineFeed) {
+      return std::string(indent, kSpace);
+    }
+    return std::string("");
+  }
+  void stringifyAnyBlockStyle(IDestination &destination,
+                              const YNode &yNode) const {
+    if (isA<String>(yNode)) {
+      if (const auto quote = YRef<String>(yNode).getQuote();
+          quote == '>' || quote == '|') {
+        destination.add("|");
+        destination.add(kLineFeed);
       }
     }
-  } else if (isA<Comment>(yNode)) {
-    destination.add("#" + YRef<Comment>(yNode).value() + kLineFeed);
-  } else if (isA<Boolean>(yNode)) {
-    destination.add(YRef<Boolean>(yNode).toString());
-  } else if (isA<Null>(yNode)) {
-    destination.add(YRef<Null>(yNode).toString());
-  } else if (isA<Hole>(yNode)) {
-    destination.add(YRef<Hole>(yNode).toString());
-  } else if (isA<Dictionary>(yNode)) {
-    for (const auto &entryYNode : YRef<Dictionary>(yNode).value()) {
-      destination.add(calculateIndent(destination, indent));
-      if (const char quote = YRef<String>(entryYNode.getKeyYNode()).getQuote();
+  }
+  void stringifyYAML(const YNode &yNode, IDestination &destination,
+                     const unsigned long indent) const {
+    if (isA<Number>(yNode)) {
+      destination.add(YRef<Number>(yNode).toString());
+    } else if (isA<String>(yNode)) {
+      if (const char quote = YRef<String>(yNode).getQuote();
           quote == kApostrophe || quote == kDoubleQuote) {
-        destination.add(
-            quote + YRef<String>(entryYNode.getKeyYNode()).toString() + quote);
+        std::string yamlString{YRef<String>(yNode).toString()};
+        if (quote == kDoubleQuote) {
+          yamlString = yamlTranslator->to(yamlString);
+        }
+        destination.add(quote + yamlString + quote);
       } else {
-        destination.add(YRef<String>(entryYNode.getKeyYNode()).toString());
+        for (const auto &line :
+             splitString(YRef<String>(yNode).toString(), kLineFeed)) {
+          destination.add(calculateIndent(destination, indent));
+          destination.add(line);
+        }
       }
-      destination.add(": ");
-      stringifyAnyBlockStyle(destination, entryYNode.getYNode());
-      if (isA<Array>(entryYNode.getYNode()) ||
-          isA<Dictionary>(entryYNode.getYNode())) {
-        destination.add(kLineFeed);
+    } else if (isA<Comment>(yNode)) {
+      destination.add("#" + YRef<Comment>(yNode).value() + kLineFeed);
+    } else if (isA<Boolean>(yNode)) {
+      destination.add(YRef<Boolean>(yNode).toString());
+    } else if (isA<Null>(yNode)) {
+      destination.add(YRef<Null>(yNode).toString());
+    } else if (isA<Hole>(yNode)) {
+      destination.add(YRef<Hole>(yNode).toString());
+    } else if (isA<Dictionary>(yNode)) {
+      for (const auto &entryYNode : YRef<Dictionary>(yNode).value()) {
+        destination.add(calculateIndent(destination, indent));
+        if (const char quote =
+                YRef<String>(entryYNode.getKeyYNode()).getQuote();
+            quote == kApostrophe || quote == kDoubleQuote) {
+          destination.add(quote +
+                          YRef<String>(entryYNode.getKeyYNode()).toString() +
+                          quote);
+        } else {
+          destination.add(YRef<String>(entryYNode.getKeyYNode()).toString());
+        }
+        destination.add(": ");
+        stringifyAnyBlockStyle(destination, entryYNode.getYNode());
+        if (isA<Array>(entryYNode.getYNode()) ||
+            isA<Dictionary>(entryYNode.getYNode())) {
+          destination.add(kLineFeed);
+        }
+        stringify(entryYNode.getYNode(), destination, indent + yamlIndentation);
+        if (!isA<Array>(entryYNode.getYNode()) &&
+            !isA<Dictionary>(entryYNode.getYNode()) &&
+            !isA<Comment>(entryYNode.getYNode())) {
+          destination.add(kLineFeed);
+        }
       }
-      stringify(entryYNode.getYNode(), destination,
-                    indent + yamlIndentation);
-      if (!isA<Array>(entryYNode.getYNode()) &&
-          !isA<Dictionary>(entryYNode.getYNode()) &&
-          !isA<Comment>(entryYNode.getYNode())) {
-        destination.add(kLineFeed);
+    } else if (isA<Array>(yNode)) {
+      for (const auto &entryYNode : YRef<Array>(yNode).value()) {
+        destination.add(calculateIndent(destination, indent) + "- ");
+        stringifyAnyBlockStyle(destination, entryYNode);
+        stringify(entryYNode, destination, indent + yamlIndentation);
+        if (destination.last() != kLineFeed) {
+          destination.add(kLineFeed);
+        }
       }
-    }
-  } else if (isA<Array>(yNode)) {
-    for (const auto &entryYNode : YRef<Array>(yNode).value()) {
-      destination.add(calculateIndent(destination, indent) + "- ");
-      stringifyAnyBlockStyle(destination, entryYNode);
-      stringify( entryYNode, destination,indent + yamlIndentation);
+    } else if (isA<Document>(yNode)) {
+      destination.add("---");
+      if (!YRef<Document>(yNode).value().empty()) {
+        stringifyAnyBlockStyle(destination, YRef<Document>(yNode)[0]);
+        if (destination.last() != kLineFeed) {
+          destination.add(kLineFeed);
+        }
+      }
+      for (const auto &entryYNode : YRef<Document>(yNode).value()) {
+        stringify(entryYNode, destination, 0);
+      }
       if (destination.last() != kLineFeed) {
         destination.add(kLineFeed);
       }
-    }
-  } else if (isA<Document>(yNode)) {
-    destination.add("---");
-    if (!YRef<Document>(yNode).value().empty()) {
-      stringifyAnyBlockStyle(destination, YRef<Document>(yNode)[0]);
-      if (destination.last() != kLineFeed) {
-        destination.add(kLineFeed);
-      }
-    }
-    for (const auto &entryYNode : YRef<Document>(yNode).value()) {
-      stringify( entryYNode, destination, 0);
-    }
-    if (destination.last() != kLineFeed) {
+      destination.add("...");
       destination.add(kLineFeed);
+    } else {
+      throw Error("Unknown YNode type encountered during stringification.");
     }
-    destination.add("...");
-    destination.add(kLineFeed);
-  } else {
-    throw Error("Unknown YNode type encountered during stringification.");
   }
-}
   // Current indentation level
   inline static unsigned long yamlIndentation{2};
   // Translator
-   std::unique_ptr<ITranslator> yamlTranslator;
+  std::unique_ptr<ITranslator> yamlTranslator;
 };
 
 } // namespace YAML_Lib
--- classes/include/implementation/stringify/JSON_Stringify.hpp
+++ classes/include/implementation/stringify/JSON_Stringify.hpp
@@ -6,8 +6,7 @@
 
 namespace YAML_Lib {
 
-class JSON_Stringify final : public IStringify
-{
+class JSON_Stringify final : public IStringify {
 public:
   JSON_Stringify() = default;
   JSON_Stringify &operator=(const JSON_Stringify &other) = delete;
@@ -15,84 +14,85 @@ public:
   JSON_Stringify &operator=(JSON_Stringify &&other) = delete;
   ~JSON_Stringify() override = default;
 
-    /// <summary>
-    /// Recursively traverse YNode structure encoding it into YAML string on
-    /// the destination stream passed in.
-    /// </summary>
-    /// <param name="yNode">YNode structure to be traversed.</param>
-    /// <param name="destination">Destination stream for stringified YAML.</param>
-    /// <param name="indent">Current print indentation.</param>
-    void stringify(const YNode &yNode, IDestination &destination, const unsigned long indent) const override
-    {
-        if (isA<Document>(yNode)) {
-            stringifyDocument(yNode, destination, 0);
-        } else if (isA<Number>(yNode)) {
-            stringifyNumber(yNode, destination);
-        } else if (isA<String>(yNode)) {
-            stringifyString(yNode, destination);
-        } else if (isA<Boolean>(yNode)) {
-            stringifyBoolean(yNode, destination);
-        } else if (isA<Null>(yNode)) {
-            stringifyNull(yNode, destination);
-        } else if (isA<Hole>(yNode)) {
-        } else if (isA<Dictionary>(yNode)) {
-            stringifyDictionary(yNode, destination);
-        } else if (isA<Array>(yNode)) {
-            stringifyAray(yNode, destination);
-        } else {
-            throw Error("Unknown YNode type encountered during stringification.");
-        }
+  /// <summary>
+  /// Recursively traverse YNode structure encoding it into JSON string on
+  /// the destination stream passed in.
+  /// </summary>
+  /// <param name="yNode">YNode structure to be traversed.</param>
+  /// <param name="destination">Destination stream for stringified JSON.</param>
+  /// <param name="indent">Current print indentation.</param>
+  void stringify(const YNode &yNode, IDestination &destination,
+                 const unsigned long indent) const override {
+    if (isA<Document>(yNode)) {
+      stringifyDocument(yNode, destination, 0);
+    } else if (isA<Number>(yNode)) {
+      stringifyNumber(yNode, destination);
+    } else if (isA<String>(yNode)) {
+      stringifyString(yNode, destination);
+    } else if (isA<Boolean>(yNode)) {
+      stringifyBoolean(yNode, destination);
+    } else if (isA<Null>(yNode)) {
+      stringifyNull(yNode, destination);
+    } else if (isA<Hole>(yNode)) {
+    } else if (isA<Dictionary>(yNode)) {
+      stringifyDictionary(yNode, destination);
+    } else if (isA<Array>(yNode)) {
+      stringifyAray(yNode, destination);
+    } else {
+      throw Error("Unknown YNode type encountered during stringification.");
     }
-
-    void stringifyDocument(const YNode &yNode, IDestination &destination, const long indent) const {
-        stringify(YRef<Document>(yNode)[0], destination, indent);
-    }
-
-  static void stringifyNumber(const YNode &yNode, IDestination &destination) {
-        destination.add(YRef<Number>(yNode).toString());
-    }
-
-    void stringifyString(const YNode &yNode, IDestination &destination) const {
-        const auto yamlString = YRef<String>(yNode).value();
-        destination.add("\""+jsonTranslator.to(yamlString)+"\"");
-    }
-
-  static void stringifyBoolean(const YNode &yNode, IDestination &destination) {
-        if (YRef<Boolean>(yNode).value()) {
-            destination.add("true");
-        } else {
-            destination.add("false");
-        }
-    }
-
-  static void stringifyNull([[maybe_unused]]const YNode &yNode, IDestination &destination) {
-        destination.add("null");
-    }
-
-    void stringifyDictionary(const YNode &yNode, IDestination &destination) const {
-        auto comma=YRef<Dictionary>(yNode).value().size()-1;
-        destination.add('{');
-        for (auto &entry : YRef<Dictionary>(yNode).value()) {
-            stringify(entry.getKeyYNode(), destination, 0);
-            destination.add(":");
-            stringify(entry.getYNode(), destination, 0);
-            if (comma-- > 0) {destination.add(","); }
-        }
-        destination.add("}");
-
-    }
-    void stringifyAray(const YNode &yNode, IDestination &destination) const {
-        auto comma=YRef<Array>(yNode).value().size()-1;
-        destination.add('[');
-        for (auto &entry : YRef<Array>(yNode).value()) {
-            stringify(entry, destination, 0);
-            if (comma-- > 0) {destination.add(","); }
-        }
-        destination.add("]");
-    }
+  }
 
 private:
+  void stringifyDocument(const YNode &yNode, IDestination &destination,
+                         const long indent) const {
+    stringify(YRef<Document>(yNode)[0], destination, indent);
+  }
+  static void stringifyNumber(const YNode &yNode, IDestination &destination) {
+    destination.add(YRef<Number>(yNode).toString());
+  }
+  void stringifyString(const YNode &yNode, IDestination &destination) const {
+    const auto yamlString = YRef<String>(yNode).value();
+    destination.add("\"" + jsonTranslator.to(yamlString) + "\"");
+  }
+  static void stringifyBoolean(const YNode &yNode, IDestination &destination) {
+    if (YRef<Boolean>(yNode).value()) {
+      destination.add("true");
+    } else {
+      destination.add("false");
+    }
+  }
+  static void stringifyNull([[maybe_unused]] const YNode &yNode,
+                            IDestination &destination) {
+    destination.add("null");
+  }
+  void stringifyDictionary(const YNode &yNode,
+                           IDestination &destination) const {
+    auto comma = YRef<Dictionary>(yNode).value().size() - 1;
+    destination.add('{');
+    for (auto &entry : YRef<Dictionary>(yNode).value()) {
+      stringify(entry.getKeyYNode(), destination, 0);
+      destination.add(":");
+      stringify(entry.getYNode(), destination, 0);
+      if (comma-- > 0) {
+        destination.add(",");
+      }
+    }
+    destination.add("}");
+  }
+  void stringifyAray(const YNode &yNode, IDestination &destination) const {
+    auto comma = YRef<Array>(yNode).value().size() - 1;
+    destination.add('[');
+    for (auto &entry : YRef<Array>(yNode).value()) {
+      stringify(entry, destination, 0);
+      if (comma-- > 0) {
+        destination.add(",");
+      }
+    }
+    destination.add("]");
+  }
+
   Default_Translator jsonTranslator;
 };
-    
-}// namespace YAML_Lib
+
+} // namespace YAML_Lib
