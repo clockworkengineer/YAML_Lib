fe0f92f910fcdcb0db026cc6c3eea0abe4438834
clockworkengineer <robert_tizzard@hotmail.com>
2025-05-23 12:46:30 +0100

    String_view phase 3.

--- classes/include/implementation/YAML_Core.hpp
+++ classes/include/implementation/YAML_Core.hpp
@@ -3,15 +3,9 @@
 // ==========
 // YAML Core
 // ==========
-
-// Interfaces
 #include "YAML_Interfaces.hpp"
-
-// Structure
 #include "YAML_Variant.hpp"
 #include "YAML_YNode.hpp"
-
-// Variants
 #include "YAML_Hole.hpp"
 #include "YAML_Array.hpp"
 #include "YAML_Boolean.hpp"
@@ -21,23 +15,13 @@
 #include "YAML_Number.hpp"
 #include "YAML_String.hpp"
 #include "YAML_Dictionary.hpp"
-
-// YNode
 #include "YAML_YNode_Reference.hpp"
 #include "YAML_YNode_Creation.hpp"
 #include "YAML_YNode_Index.hpp"
-
-// IO
 #include "YAML_Sources.hpp"
 #include "YAML_Destinations.hpp"
-
-// Error
 #include "YAML_Error.hpp"
-
-// Defaults
+#include "YAML_Converter.hpp"
 #include "Default_Translator.hpp"
 #include "Default_Parser.hpp"
 #include "Default_Stringify.hpp"
-
-// Converter
-#include "YAML_Converter.hpp"
