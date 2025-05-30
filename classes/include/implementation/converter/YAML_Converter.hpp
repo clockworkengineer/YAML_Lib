6668dd96c607f6640b5aa8cea88af12a5443722a
clockworkengineer <robert_tizzard@hotmail.com>
2025-05-30 17:41:29 +0100

    refactor converter.

--- classes/include/implementation/converter/YAML_Converter.hpp
+++ classes/include/implementation/converter/YAML_Converter.hpp
@@ -1,5 +1,6 @@
 #pragma once
 namespace YAML_Lib {
+[[nodiscard]] std::string toUtf8(char16_t utf16);
 [[nodiscard]] std::u16string toUtf16(const std::string_view &utf8);
 [[nodiscard]] std::string toUtf8(const std::u16string_view &utf16);
 } // namespace YAML_Lib
